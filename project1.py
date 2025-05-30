93eacb2fe236e58caacb4e9a89a366b04372c028
FranciscoJavierRAM <pacorro-2014@hotmail.com>
2025-05-29 20:57:29 -0400

    Add files via upload

--- project1.py
+++ project1.py
@@ -9,19 +9,23 @@ hour = int(input("What is the current hour?\n" \
 "(please type a whole number)\n"))
 padded_hour = f"{hour:02}"
 hour_min = hour * 60
-minute = int(input("Now the minutes\n"))
+minute = int(input("Now the minutes:\n"))
 padded_minute = f"{minute:02}"
+seconds = int(input("Now the seconds:\n"))
+padded_seconds = f"{seconds:02}"
 am_pm = input("Is it currently am or pm: ")
 
-print(f"Your time is currently {padded_hour}:{padded_minute}{am_pm}\n")
+print(f"Your time is currently {padded_hour}:{padded_minute}:{padded_seconds}{am_pm}\n")
 
 future_time = int(input(" How many minutes will you want to travel into the future?\n" \
 "(Please type a positive whole number\n"))
 total_minutes = ((hour_min + minute) + future_time)
 
 if total_minutes >= 720:
-    if am_pm = "am":
+    if am_pm == "am":
         am_pm = "pm"
+    elif am_pm == "pm":
+        am_pm = "am"
 if total_minutes >= 780:
    total_minutes = total_minutes - 720
 hours = total_minutes // 60
@@ -29,7 +33,20 @@ hours_padded = f"{hours:02}"
 remaining = total_minutes % 60
 minutes_padded = f"{remaining:02}"
 
-print(f"your new time is {hours_padded}:{minutes_padded}{am_pm}")
+print(f"Your new time is {hours_padded}:{minutes_padded}:{padded_seconds}{am_pm}!\n")
+seconds_forward = int(input("Now lets travel in seconds, how many seconds will you travel into the future?:\n"))
+total_seconds = (total_minutes * 60) + seconds_forward + seconds
+print (total_seconds)
+if total_seconds >= 46800:
+   total_seconds = total_seconds - 43200
+hours = total_seconds // 3600
+hours_padded = f"{hours:02}"
+total_seconds = total_seconds % 3600
+minutes = total_seconds // 60
+minutes_padded = f"{minutes:02}"
+seconds = total_seconds % 60
+seconds_padded = f"{seconds:02}"
+print(f"Your new time is {hours_padded}:{minutes_padded}:{seconds_padded}{am_pm}!\n")
 
 
 
